7f077b91e35438ef35c7658e3543ea56d82df97c
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-11-14 15:10:30 +0530

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,10 @@
 'use strict';
 
-const navigationOpenButton = document.querySelector('.open-btn');
-const navigationCloseButton = document.querySelector('.close-btn');
 const header = document.querySelector('header');
+const mobileNavButton = document.querySelector('.header__mobile-nav-toggle');
 const overlay = document.querySelector('.overlay');
 
-const openNav = () => {
-    header.classList.add('nav-open');
-    overlay.classList.remove('hidden');
-}
-
-const closeNav = () => {
-    header.classList.remove('nav-open');
-    overlay.classList.add('hidden');
-}
-
-navigationOpenButton.addEventListener('click', openNav);
-navigationCloseButton.addEventListener('click', closeNav);
-document.addEventListener('keydown', (event) => {
-    if (header.classList.contains('nav-open')) {
-        if (event.key === 'Escape') {
-            closeNav();
-        }
-    }
+mobileNavButton.addEventListener('click', () => {
+    header.classList.toggle('nav-open');
+    overlay.classList.toggle('hidden');
 })
-
