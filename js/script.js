22e10a39a3e7a767c09498d7e66610617e2532bc
Krutagna <kpatel.kp7016@gmail.com>
2023-01-10 09:57:28 +0530

    .

--- js/script.js
+++ js/script.js
@@ -1,32 +1,45 @@
 'use strict';
 
-const header = document.querySelector('header');
-const mobileNavButton = document.querySelector('.header__mobile-nav-toggle');
+const navList = document.querySelector('.header__nav-list');
+const navToggle = document.querySelector('.header__mobile-nav-toggle');
 const overlay = document.querySelector('.overlay');
 
-// Function for hiding mobile navigation
-const hideMobileNavigation = () => {
-    header.classList.remove('nav-open');
-    overlay.classList.add('hidden');
-};
-
-// When the user clicks on navigation button
-mobileNavButton.addEventListener('click', () => {
-    header.classList.toggle('nav-open');
-    overlay.classList.toggle('hidden');
-});
-
-// When the user clicks on escape key
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-        hideMobileNavigation();
+navToggle.addEventListener('click', () => {
+    const visibility = navList.getAttribute('data-visible');
+    if (visibility === 'false') {
+        navList.setAttribute('data-visible', 'true');
+        navToggle.setAttribute('aria-expanded', 'true');
+        overlay.classList.remove('hidden');
+    } else {
+        navList.setAttribute('data-visible', 'false');
+        navToggle.setAttribute('aria-expanded', 'false');
+        overlay.classList.add('hidden');
     }
-});
+})
 
-// When the width is greater than 768 pixels
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-        hideMobileNavigation()
-    }
-});
+// // Function for hiding mobile navigation
+// const hideMobileNavigation = () => {
+//     header.classList.remove('nav-open');
+//     overlay.classList.add('hidden');
+// };
+//
+// // When the user clicks on navigation button
+// mobileNavButton.addEventListener('click', () => {
+//     header.classList.toggle('nav-open');
+//     overlay.classList.toggle('hidden');
+// });
+//
+// // When the user clicks on escape key
+// document.addEventListener('keydown', (event) => {
+//     if (event.key === 'Escape') {
+//         hideMobileNavigation();
+//     }
+// });
+//
+// // When the width is greater than 768 pixels
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth > 768) {
+//         hideMobileNavigation()
+//     }
+// });
 
