1d897850358308610f79a87113fe0839218817b4
Krutagna <kpatel.kp7016@gmail.com>
2023-01-10 10:11:14 +0530

    .

--- js/script.js
+++ js/script.js
@@ -17,29 +17,25 @@ navToggle.addEventListener('click', () => {
     }
 })
 
-// // Function for hiding mobile navigation
-// const hideMobileNavigation = () => {
-//     header.classList.remove('nav-open');
-//     overlay.classList.add('hidden');
-// };
-//
-// // When the user clicks on navigation button
-// mobileNavButton.addEventListener('click', () => {
-//     header.classList.toggle('nav-open');
-//     overlay.classList.toggle('hidden');
-// });
-//
-// // When the user clicks on escape key
-// document.addEventListener('keydown', (event) => {
-//     if (event.key === 'Escape') {
-//         hideMobileNavigation();
-//     }
-// });
-//
-// // When the width is greater than 768 pixels
-// window.addEventListener('resize', () => {
-//     if (window.innerWidth > 768) {
-//         hideMobileNavigation()
-//     }
-// });
+// Function for hiding mobile navigation
+const hideMobileNavigation = () => {
+    navList.setAttribute('data-visible', 'false');
+    navToggle.setAttribute('aria-expanded', 'false');
+    overlay.classList.add('hidden');
+};
+
+
+// When the user clicks on escape key
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        hideMobileNavigation();
+    }
+});
+
+// When the width is greater than 768 pixels
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        hideMobileNavigation()
+    }
+});
 
